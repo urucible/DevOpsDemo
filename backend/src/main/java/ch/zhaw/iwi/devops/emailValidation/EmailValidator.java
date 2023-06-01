6c619209d12b4b13481a417db6c71d207e0700fa
urucible <urucible@students.zhaw.ch>
2023-06-02 00:43:53 +0200

    Java classe nun final

--- backend/src/main/java/ch/zhaw/iwi/devops/emailValidation/EmailValidator.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/emailValidation/EmailValidator.java
@@ -1,11 +1,14 @@
 package ch.zhaw.iwi.devops.emailValidation;
 
-import java.util.function.BooleanSupplier;
+import java.util.regex.Pattern;
 
 public class EmailValidator {
 
-    public BooleanSupplier validateEmail(String email) {
-        return null;
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+    private static final Pattern pattern = Pattern.compile(EMAIL_REGEX);
+
+    public boolean validateEmail(String email) {
+        return pattern.matcher(email).matches();
     }
 
 }
