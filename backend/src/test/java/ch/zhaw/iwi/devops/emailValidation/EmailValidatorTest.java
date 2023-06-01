3f14a5d940d46400f7a629f27c1ee1e2e5a832f9
urucible <urucible@students.zhaw.ch>
2023-06-02 00:35:19 +0200

    alle tests geschrieben

--- backend/src/test/java/ch/zhaw/iwi/devops/emailValidation/EmailValidatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/emailValidation/EmailValidatorTest.java
@@ -9,6 +9,68 @@ public class EmailValidatorTest {
     public void emailValidatorTestOne(){
         EmailValidator validator = new EmailValidator();
         String email = "";
-        Assertions.assertFalse(validator.validateEmail(email), null);
+        Assertions.assertFalse(validator.validateEmail(email));
     }
+
+    @Test
+    public void emailValidatorTestTwo(){
+        EmailValidator validator = new EmailValidator();
+        String email = "test@";
+        Assertions.assertFalse(validator.validateEmail(email));
+    }
+
+    @Test
+    public void emailValidatorTestThree(){
+        EmailValidator validator = new EmailValidator();
+        String email = "@test";
+        Assertions.assertFalse(validator.validateEmail(email));
+    }
+
+    @Test
+    public void emailValidatorTestFour(){
+        EmailValidator validator = new EmailValidator();
+        String email = "@test.com";
+        Assertions.assertFalse(validator.validateEmail(email));
+    }
+    @Test
+    public void emailValidatorTestFive(){
+        EmailValidator validator = new EmailValidator();
+        String email = "@.com";
+        Assertions.assertFalse(validator.validateEmail(email));
+    }
+
+    @Test
+    public void emailValidatorTestSix(){
+        EmailValidator validator = new EmailValidator();
+        String email = "testmail.com";
+        Assertions.assertFalse(validator.validateEmail(email));
+    }
+    @Test
+    public void emailValidatorTestSeven(){
+        EmailValidator validator = new EmailValidator();
+        String email = "test@mail.com";
+        Assertions.assertFalse(validator.validateEmail(email));
+    }
+    @Test
+    public void emailValidatorTestEight(){
+        EmailValidator validator = new EmailValidator();
+        String email = "test @mail.com";
+        Assertions.assertFalse(validator.validateEmail(email));
+    }
+
+    @Test
+    public void emailValidatorTestNine(){
+        EmailValidator validator = new EmailValidator();
+        String email = "test@mail.com";
+        Assertions.assertTrue(validator.validateEmail(email));
+    }
+
+    @Test
+    public void emailValidatorTestTen(){
+        EmailValidator validator = new EmailValidator();
+        String email = "test123@mail.com";
+        Assertions.assertTrue(validator.validateEmail(email));
+    }
+
+
 }
