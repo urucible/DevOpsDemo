c76e348c9da5df6bbdf0bc2e4a1e55e37f30f61c
urucible <urucible@students.zhaw.ch>
2023-06-02 00:35:34 +0200

    Klassenerzeugung und methode validateEmail

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/emailValidation/EmailValidator.java
@@ -0,0 +1,11 @@
+package ch.zhaw.iwi.devops.emailValidation;
+
+import java.util.function.BooleanSupplier;
+
+public class EmailValidator {
+
+    public BooleanSupplier validateEmail(String email) {
+        return null;
+    }
+
+}
