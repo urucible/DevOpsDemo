39264aee719adef989998cb3d5fd82e340cdc000
urucible <urucible@students.zhaw.ch>
2023-06-04 20:36:05 +0200

    testcoverage of 100%

--- backend/src/test/java/ch/zhaw/iwi/devops/applicationEntry/ApplicationEntryTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/applicationEntry/ApplicationEntryTest.java
@@ -14,5 +14,25 @@ public class ApplicationEntryTest {
         entry.setAppName("AppName");
         assertEquals(entry.getAppName(), "AppName");
     }
+
+    @Test
+    public void testgetId(){
+        ApplicationEntry entry = new ApplicationEntry(8,"demoAppName", "0.0.1", "testchange set");
+        assertEquals(entry.getId(), 8);
+    }
+
+    @Test
+    public void testSetAppVersion(){
+        ApplicationEntry entry = new ApplicationEntry();
+        entry.setAppVersion("0.0.1");
+        assertEquals(entry.getAppVersion(), "0.0.1");
+    }
+
+    @Test
+    public void testSetChangeSet(){
+        ApplicationEntry entry = new ApplicationEntry();
+        entry.setChangeSet("Change set to be checked");
+        assertEquals(entry.getChangeSet(), "Change set to be checked");
+    }
     
 }
